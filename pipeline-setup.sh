8a106d61180281afc056edc8a6a0e41993849802
Tariq Islam <tislam@redhat.com>
2016-10-05 20:00:40 -0400

    expose service on deploy

--- pipeline-setup.sh
+++ pipeline-setup.sh
@@ -4,6 +4,7 @@
 oc login https://192.168.122.124:8443 --insecure-skip-tls-verify -u openshift-dev -p devel
 oc new-project app-dev --display-name="Application Development Environment"
 oc new-app --name=bluegreen https://github.com/tariq-islam/bluegreen#ocp33-pipeline
+oc expose service bluegreen
 oc new-app jenkins-ephemeral -p JENKINS_PASSWORD=password
 oc create -f https://raw.githubusercontent.com/tariq-islam/bluegreen/ocp33-pipeline/php-bluegreen-app-pipeline-bc.yml
 oc new-project app-qa --display-name="Application QA Environment"
