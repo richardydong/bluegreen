d0f6212cc3bb035414570086c923912b1a028008
Tariq Islam <tislam@redhat.com>
2016-10-05 20:08:12 -0400

    push all

--- pipeline-setup.sh
+++ pipeline-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-oc login https://192.168.122.124:8443 --insecure-skip-tls-verify -u openshift-dev -p devel
+# Uncomment the following line and specify your master:port to log into if you want to automate the login and not have to login prior to running this script
+# oc login https://192.168.122.124:8443 --insecure-skip-tls-verify -u openshift-dev -p devel
 oc new-project app-dev --display-name="Application Development Environment"
 oc new-app --name=bluegreen https://github.com/tariq-islam/bluegreen#ocp33-pipeline
 oc expose service bluegreen
