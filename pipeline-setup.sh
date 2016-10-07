0d333841834da5153472ceb12fdd7f514bcdaf73
Tariq Islam <tislam@redhat.com>
2016-10-07 19:22:15 -0400

    parameterizing a couple of things

--- pipeline-setup.sh
+++ pipeline-setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo "Please enter your OpenShift hostname:port (https://<hostname>:<port>): "
+
+read hostname
+
+echo "Enter your username: "
+
+read username
 
 # Uncomment the following line and specify your master:port to log into if you want to automate the login and not have to login prior to running this script
-# oc login https://192.168.122.124:8443 --insecure-skip-tls-verify -u openshift-dev -p devel
+oc login "$hostname" --insecure-skip-tls-verify -u "$username"
 oc new-project app-dev --display-name="Application Development Environment"
 oc new-app --name=bluegreen https://github.com/tariq-islam/bluegreen#ocp33-pipeline
 oc expose service bluegreen
