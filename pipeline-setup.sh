cdf70d10a2c05fa135de1eb3fe561590d1f0eb09
Tariq Islam <tislam@redhat.com>
2016-11-30 11:58:46 -0500

    going green

--- pipeline-setup.sh
+++ pipeline-setup.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-echo "Please enter your OpenShift hostname:port (https://<hostname>:<port>). The default used is the CDK @ https://10.1.2.2:8443: "
-
-read hostname
+echo "Setting up environments and pipeline"
 
 jenkins_image="jenkins-ephemeral"
 
-if [ -z "$hostname" ]; then
-	hostname="https://10.1.2.2:8443"
-	jenkins_image="jenkins"
-fi
+hostname="https://192.168.122.71:8443"
 
-echo "Enter your username (default is openshift-dev): "
+username="openshift-dev"
 
-read username
+repository_path="https://github.com/tariq-islam/bluegreen#ocp33-pipeline-ti"
 
-if [ -z "$username" ]; then
-	username="openshift-dev"
-fi
-
-echo "Enter the url for your repository. If it's a specific branch you'd like to use, please append the branch to the url with '#<branch_name>' [default: https://github.com/tariq-islam/bluegreen#ocp33-pipeline-ti]: "
-
-read repository_path
-
-if [ -z "$repository_path" ]; then
-	repository_path="https://github.com/tariq-islam/bluegreen#ocp33-pipeline-ti"
-	echo "Using default repository at : " $repository_path
-fi
-
+echo "Logging into OpenShift"
 oc login "$hostname" --insecure-skip-tls-verify -u "$username"
+
+echo "Creating shared development environment (app-dev)"
 oc new-project app-dev --display-name="Application Development Environment"
 oc new-app --name=bluegreen "$repository_path"
 oc expose service bluegreen
