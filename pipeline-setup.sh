50499ca060a270b6d67476899628da5ce9bf27ac
Tariq Islam <tislam@redhat.com>
2017-02-01 11:16:40 -0500

    removed manual provision of jenkins

--- pipeline-setup.sh
+++ pipeline-setup.sh
@@ -4,7 +4,7 @@ echo "Setting up environments and pipeline"
 
 jenkins_image="jenkins-ephemeral"
 
-hostname="https://192.168.122.71:8443"
+hostname="https://192.168.122.121:8443"
 
 username="openshift-dev"
 
@@ -17,7 +17,6 @@ echo "Creating shared development environment (app-dev)"
 oc new-project app-dev --display-name="Application Development Environment"
 oc new-app --name=bluegreen "$repository_path"
 oc expose service bluegreen
-oc new-app "$jenkins_image" -p JENKINS_PASSWORD=password
 oc create -f https://raw.githubusercontent.com/tariq-islam/bluegreen/ocp33-pipeline-ti/php-bluegreen-app-pipeline-bc.yml
 oc new-project app-qa --display-name="Application QA Environment"
 oc new-project app-prod --display-name="Application Production Environment"
