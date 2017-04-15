5f6469910c1e16809763dd83fc5fbfdd31bf5666
tariq-islam <tariq.m.islam@gmail.com>
2017-04-15 14:31:35 -0400

    making updates to how resources are handled

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 oc project app-dev
-oc delete all --all
+oc delete all --all --now
 oc project app-qa
-oc delete all --all
+oc delete all --all --now
 oc project app-prod
-oc delete all --all
+oc delete all --all --now
 oc delete project app-dev app-qa app-prod
